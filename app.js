f8eadfe930524939d5a18ddebf00ad98b1e6001a
cobbland <jacobdensford@proton.me>
2025-10-16 14:57:38 -0400

    Add cookie max age

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(session({
     saveUninitialized: false,
     cookies: {
         secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24,
     },
 }));
 app.use(passport.initialize());
